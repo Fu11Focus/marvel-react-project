daf7562eb273aea875909b2b276ff357aed84749
Fu11Focus <exemple@gmail.com>
2022-06-01 01:28:43 +0300

    layout

---
+++ src/components/app/App.js
@@ -0,0 +1,27 @@
+import AppHeader from "../appHeader/AppHeader";
+import RandomChar from "../randomChar/RandomChar";
+import CharList from "../charList/CharList";
+import CharInfo from "../charInfo/CharInfo";
+
+import decoration from '../../resources/img/vision.png';
+
+import getCharacters from "../servises/getApi";
+
+const App = () => {
+    getCharacters();
+    return (
+        <div className="app">
+            <AppHeader/>
+            <main>
+                <RandomChar/>
+                <div className="char__content">
+                    <CharList/>
+                    <CharInfo/>
+                </div>
+                <img className="bg-decoration" src={decoration} alt="vision"/>
+            </main>
+        </div>
+    )
+}
+
+export default App;
