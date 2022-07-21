7c22ad34fb5d384ad4b32f849bc0ebd0aeebd527
Fu11focus <evgen_k9@ukr.net>
2022-07-21 18:13:46 +0300

    add router dom

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,51 +1,63 @@
 import { useState } from "react";
-import { Route, BrowserRouter as Router} from "react-router-dom";
-import useMarvelServices from "../services/getApi";
+import { Route, BrowserRouter as Router, Routes} from "react-router-dom";
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
 import ComicsList from "../comicsList/ComicsList";
-import SingleComic from "../singleComic/SingleComic";
 import AppBanner from "../appBanner/AppBanner"; 
 
 import decoration from '../../resources/img/vision.png';
 
 const App = () => {
+    
+    
+    return (
+        <Router>
+            <div className="app">
+                <AppHeader/>
+                <main>
+                    <Routes>
+                        <Route path="/" element={<MainPage/>}/>
+                        <Route path="/comics" element={<ComicsPage/>}/>
+                    </Routes>
+                    <img className="bg-decoration" src={decoration} alt="vision"/>
+                </main>
+            </div>
+        </Router>
+    )
+}
+
+export default App;
+
+const MainPage = () => {
     const [charId, setCharId] = useState(null);
 
     const onCharSelected = (charId) => {
         setCharId(charId);
     }
-    
+
     return (
-        <div className="app">
-            <AppHeader/>
-            <main>
-                <Router>
-                    <Route path="/">
-                        <ErrorBoundary>
-                            <RandomChar/>
-                        </ErrorBoundary>
-                        <div className="char__content">
-                            <ErrorBoundary>
-                                <CharList onCharSelected={onCharSelected} charId={charId}/>
-                            </ErrorBoundary>
-                            <ErrorBoundary>
-                                <CharInfo charId={charId}/>
-                            </ErrorBoundary>
-                        </div>
-                    </Route>
-                    <Route path="/comics">
-                        <AppBanner/>
-                        <ComicsList/>
-                    </Route>
-                <img className="bg-decoration" src={decoration} alt="vision"/>
-                </Router>
-            </main>
-        </div>
+        <>
+            <ErrorBoundary>
+                <RandomChar/>
+            </ErrorBoundary>
+            <div className="char__content">
+                <ErrorBoundary>
+                    <CharList onCharSelected={onCharSelected} charId={charId}/>
+                </ErrorBoundary>
+                <ErrorBoundary>
+                    <CharInfo charId={charId}/>
+                </ErrorBoundary>
+            </div>
+        </>
     )
 }
 
-export default App;
+const ComicsPage = () => {
+    return(<>
+        <AppBanner/>
+        <ComicsList/> 
+    </>)
+}
--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -1,5 +1,5 @@
 import './appHeader.scss';
-import {Link} from "react-router-dom";
+import {NavLink, Link} from "react-router-dom";
 
 const AppHeader = () => {
     return (
@@ -11,9 +11,9 @@ const AppHeader = () => {
             </h1>
             <nav className="app__menu">
                 <ul>
-                    <li><Link to="/">Characters</Link></li>
+                    <li><NavLink end to="/" style={({isActive}) => ({color: isActive ? "#9f0013" : "inherit"})}>Characters</NavLink></li>
                     /
-                    <li><Link to="/comics">Comics</Link></li>
+                    <li><NavLink end to="/comics" style={({isActive}) => ({color: isActive ? "#9f0013" : "inherit"})}>Comics</NavLink></li>
                 </ul>
             </nav>
         </header>
