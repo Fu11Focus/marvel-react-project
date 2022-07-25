700959ecb94cd68cd53c6b85dcf1982c29c2c692
Fu11focus <evgen_k9@ukr.net>
2022-07-25 08:02:54 +0300

    add 404 page

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,6 @@
 import { Route, BrowserRouter as Router, Routes} from "react-router-dom";
 import AppHeader from "../appHeader/AppHeader";
-import {ComicsPage, MainPage, SingleComicPage} from "../pages/Index";
+import {ComicsPage, MainPage, SingleComicPage, Page404} from "../pages/Index";
 
 import decoration from '../../resources/img/vision.png';
 
@@ -15,6 +15,7 @@ const App = () => {
                         <Route path="/" element={<MainPage/>}/>
                         <Route path="/comics" element={<ComicsPage/>}/>
                         <Route path="/comics/:comicId" element={<SingleComicPage/>}/>
+                        <Route path="*" element={<Page404/>}/>
                     </Routes>
                     <img className="bg-decoration" src={decoration} alt="vision"/>
                 </main>
